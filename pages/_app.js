56ec3bbeecf7515c5c266382217566af12eda033
한주연 <hjy2859@naver.com>
2023-04-16 13:19:35 +0900

    메인 컴포넌트 위치변경

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import { PortfolioProvider } from "@/context/context";
 import { prefix } from "@/config/config";
+import { PortfolioProvider } from "@/context/context";
 
 import "@/styles/globals.css";
-
 export default function App({ Component, pageProps }) {
   return (
-    <PortfolioProvider value={{ prefix }}>
+    <PortfolioProvider value={prefix}>
       <Component {...pageProps} />
     </PortfolioProvider>
   );
